dcab510b5b7950ac9e2701db691580ad8462a14c
Robro313 <singh.rohil@northeastern.edu>
2024-12-05 23:39:08 -0500

    Battle Rule

--- src/cs3500/threetrios/model/battlerules/BattleRule.java
+++ src/cs3500/threetrios/model/battlerules/BattleRule.java
@@ -1,14 +1,10 @@
 package cs3500.threetrios.model.battlerules;
 
+import cs3500.threetrios.model.VariantThreeTriosModel;
+import cs3500.threetrios.model.card.Card;
 import cs3500.threetrios.model.cell.Cell;
 import java.util.List;
 
 public interface BattleRule {
-
-  List<List<Cell>> battle(List<List<Cell>> grid, List<Integer> attackingRows,
-                          List<Integer> attackingCols);
-
-  boolean processBattle(Cell attackingCard, Cell north, Cell south, Cell east, Cell west);
-
-
+    List<List<Cell>> applyRule(Card placedCard, int row, int col, VariantThreeTriosModel model);
 }
